e79de9b71366c2e5355d412a942f6674f6cf894b
Adrian Leung <adrian.leungssw@gmail.com>
2021-03-04 16:49:45 -0800

    closer

--- Classwork/classwork04/classwork04.c
+++ Classwork/classwork04/classwork04.c
@@ -1,74 +1,47 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
+#include "main.h"
 
 const int timeSlice = 40;
-  bool threadA = true;
-  bool threadB = true;
-  bool threadC = true;
-  bool threadD = true;
-int main(int argc, char *argv[]){
-  char* A = argv[1];
-  int aTime = atoi(argv[2]);
-  char* B = argv[3];
-  int bTime = atoi(argv[4]);
-  char* C = argv[5];
-  int cTime = atoi(argv[6]);
-  char* D = argv[7];
-  int dTime = atoi(argv[8]);
-for(int i = 0; i < argc; i++){
-    printf("%s",argv[i]);
-}
-}
-//   while(threadA || threadB || threadC || threadD ){
-//     if(threadA){
-//       if(aTime > timeSlice){
-//           aTime -= timeSlice;
-//           printf("A:%d:%d\n", timeSlice, aTime);
-//       }else if(timeSlice > aTime && aTime > 0){
-//           printf("A:%d:Done\n", aTime);
-//           threadA = false;
-//       }
-//     }
-//     if(threadB){
-//       if(bTime > timeSlice){
-//           bTime -= timeSlice;
-//           printf("B:%d:%d\n", timeSlice, bTime);
-//       }else if(timeSlice > aTime && aTime > 0){
-//           printf("B:%d:Done\n", bTime);
-//           threadB = false;
-//       }
-//     }
-//     if(threadC){
-//       if(cTime > timeSlice){
-//           cTime -= timeSlice;
-//           printf("C:%d:%d\n", timeSlice, cTime);
-//       }else if(timeSlice > cTime && cTime > 0){
-//           printf("C:%d:Done\n", cTime);
-//           threadC = false;
-//       }
-//     }
-//     if(threadD){
-//       if(dTime > timeSlice){
-//           dTime -= timeSlice;
-//           printf("D:%d:%d\n", timeSlice, dTime);
-//       }else if(timeSlice > dTime && dTime > 0){
-//           printf("D:%d:Done\n", dTime);
-//           threadD = false;
-//       }
-//     }                      
-//   }
-//   return 0;
-// }
+bool threadA = true;
+bool threadB = true;
+bool threadC = true;
+bool threadD = true;
 
 
-bool run(char thread, int time){
-    if(time > timeSlice){
-        time -= timeSlice;
-        printf("%c:%d:%d", thread, timeSlice, time);
-        return true;
-    }else if(timeSlice > time && time > 0){
-        printf("%c:%d:Done", thread, timeSlice-time);
-        return false;
+int main(int argc, char * argv[]){
+  char A = 'A';
+  int aTime = 100;
+  char B = 'B';
+  int bTime = 200;
+  char C = 'C';
+  int cTime = 130;
+  char D = 'D';
+  int dTime = 74;
+
+  while(threadA || threadB || threadC || threadD ){
+    if(threadA){
+      run(A, &aTime, &threadA);
+     }
+     if(threadB){
+       run(B, &bTime, &threadB);
+     }
+     if(threadC){
+       run(C, &cTime, &threadC);
+     }
+     if(threadD){
+       run(D, &dTime, &threadD);              
     }
+  }
+return 0;
+}
+
+void run(char A, int *time, bool *thread){
+  if(*time > timeSlice){
+    *time -= timeSlice;
+    printf("%c:%d:%d\n", A, timeSlice, *time);
+  }else if(timeSlice > *time && *time > 0){
+    printf("%c:%d:Done\n", A, *time);
+    *thread = false;
+  }
 }
